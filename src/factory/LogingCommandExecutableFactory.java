4e0777d11a6a75a9d233c6fad138c0c7569ee5f7
lavanal <alavanov@mail.ru>
2023-03-03 09:43:16 +0300

    Changed Added Executable new

--- src/factory/LogingCommandExecutableFactory.java
+++ src/factory/LogingCommandExecutableFactory.java
@@ -1,5 +1,15 @@
 package factory;
 
-public class LogingCommandExecutableFactory extends CommandExecutableFactory{
+import terminal.command.Command;
+import terminal.executables.CommandExecutable;
+import zoo.Zoo;
 
+public class LogingCommandExecutableFactory extends CommandExecutableFactory{
+    @Override
+    public CommandExecutable createExecutable(Zoo zoo, Command command) {
+        System.out.println("Star");
+        CommandExecutable newCE = super.createExecutable(zoo, command);
+        System.out.println("End");
+        return newCE;
+    }
 }
